1035b6adc48ce6abfc0210e26ca02329a94df78e
Xita Rps <xitarps@gmail.com>
2022-02-17 00:07:58 -0300

    [Feature]: Add Hello and context[:time] to response and cli

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,10 +8,12 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :test_field, String, null: false,description: "An example field added by the generator" do
+      argument :name, String, required: true
+    end
+    def test_field(name:)
+      Rails.logger.info context[:time]
+      "Hello #{name}"
     end
   end
 end
