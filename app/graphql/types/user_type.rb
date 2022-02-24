b706c9c139a3c7bfc39dffb4041b5809278fe72e
Xita Rps <xitarps@gmail.com>
2022-02-23 23:06:26 -0300

    [Feature]: Add visibility Authorization/Access
    - Predicate method: self.visible?(belongs to class, ends with question
      mark and return a boolean value)

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,4 +4,7 @@ class Types::UserType < Types::BaseObject
   field :email,         String,   null: true
   field :is_superadmin, Boolean,  null: true
 
+  def self.visible?(context)
+    !!context[:current_user]
+  end
 end
