450470a3eb0cc43eb23cddc3aae907e344413423
Xita Rps <xitarps@gmail.com>
2022-02-23 22:11:41 -0300

    [Feature]: Add User model
     - rails g model User email:string password_digest:string is_superadmin:boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_031219) do
+ActiveRecord::Schema.define(version: 2022_02_24_011113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2022_02_17_031219) do
     t.string "last_name"
     t.integer "birth_year"
     t.boolean "is_alive"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "is_superadmin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
