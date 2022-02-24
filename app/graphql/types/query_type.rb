0ede58d695c65229432be2e7cab64a7201b7a5e3
Xita Rps <xitarps@gmail.com>
2022-02-23 22:54:03 -0300

    [Feature]: Add Authorization

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,13 +8,13 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false, description: "An example field added by the generator" do
-      argument :name, String, required: true
-    end
-    def test_field(name:)
-      Rails.logger.info context[:time]
-      "Hello #{name}"
-    end
+    # field :test_field, String, null: false, description: "An example field added by the generator" do
+    #   argument :name, String, required: true
+    # end
+    # def test_field(name:)
+    #   Rails.logger.info context[:time]
+    #   "Hello #{name}"
+    # end
 
     field :author, Types::AuthorType , null: true, description: "Returns one Author instance" do
       argument :first_name, String, required: true
@@ -38,6 +38,11 @@ module Types
       if user = User.where(email: email).first.authenticate(password)
         user.sessions.create.key
       end
+    end
+
+    field :current_user, Types::UserType, null: true, description: "The current logged user"
+    def current_user
+      context[:current_user]
     end
   end
 end
